406de2ca403857724eefd3085bdc9f7da71958a2
ayushmishra <mail81mitthu@gmail.com>
2022-06-25 21:46:00 +0530

    intial commit

---
+++ CMakeFiles/AutoFlappy.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ayu/Code/random/AutoFlappy/src/main.cpp" "/home/ayu/Code/random/AutoFlappy/CMakeFiles/AutoFlappy.dir/src/main.cpp.o"
+  "/home/ayu/Code/random/AutoFlappy/src/utils.cpp" "/home/ayu/Code/random/AutoFlappy/CMakeFiles/AutoFlappy.dir/src/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/AutoFlappy.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "AutoFlappy"
+  "AutoFlappy.pdb"
+  "CMakeFiles/AutoFlappy.dir/src/main.cpp.o"
+  "CMakeFiles/AutoFlappy.dir/src/utils.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AutoFlappy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
